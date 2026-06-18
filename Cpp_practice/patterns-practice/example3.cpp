690caba727019f44f44a5b7fb65e91394fa3d087
TheMohitBuilds <mohit120577@gmail.com>
2026-06-18 20:43:32 +0530

    Added pattern practice files

--- Cpp_practice/patterns-practice/example3.cpp
+++ Cpp_practice/patterns-practice/example3.cpp
@@ -1,8 +1,9 @@
+
 #include<iostream>
 using namespace std;
 
 int main() {
-/*
+
 int n;
 cin >> n;
 
@@ -12,37 +13,18 @@ int i = 1;
    int j = 1;
    while(j<=n) {
 
-   cout<<i;
-   j = j + 1;
-   }
-   cout<<endl;
-   i = i + 1;
-
-    }*/
-
-
-
-    int n;
-cin >> n;
-
-int i = 1;
-    while(i<=n) {
-    
-   int j = 1;
-   while(j<=n) {
-
-   cout<<j;
+   cout<<n-j+1;
    j = j + 1;
    }
    cout<<endl;
    i = i + 1;
 
     }
+
+
 }
 
 
-
-
-
+    
 
 
