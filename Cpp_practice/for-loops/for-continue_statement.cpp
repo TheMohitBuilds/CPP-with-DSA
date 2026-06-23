457065aa14237f70d1dda900400d301a82aa845e
TheMohitBuilds <mohit120577@gmail.com>
2026-06-23 22:30:02 +0530

    Added continue statement practice folder

---
+++ Cpp_practice/for-loops/for-continue_statement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+for(int i = 0; i<=1;i++) {
+cout << "CPP-with-DSA" << endl;
+cout << "Python-for-AI" << endl;
+cout << "Full-Stack-Web-Development" << endl;
+
+continue;
+}
+cout << "[TheMohitBuilds~]" << endl;
+
+}
