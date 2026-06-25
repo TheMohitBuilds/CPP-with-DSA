3b6906ebb963434c5955f5a1c404a15e62b7f253
TheMohitBuilds <mohit120577@gmail.com>
2026-06-25 13:55:53 +0530

    Added switch statement example and mini-calculator practice folder

---
+++ Cpp_practice/switch_statements,function/switch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+int num = 1;
+cout << endl;
+
+switch( num ) {
+
+case 1: cout << "First" << endl;
+break;
+
+case 2: cout << "Second" << endl;
+break;
+
+default: cout << "It is a default case" << endl;
+
+}
+
+cout << endl;
+
+
+
+
+}
