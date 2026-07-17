5cbb6e9d4905cbeb480661759b2ad250485c06f5
TheMohitBuilds <mohit120577@gmail.com>
2026-07-17 10:41:00 +0530

    Added sum_array file in Array_introduction folder

---
+++ Cpp_practice/CPP-Arrays_introduction/sum_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int sumArr(int arr[],int n){
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int main() {
+
+    int size;
+    cin >> size;
+     
+    int num[100];
+
+    for(int i = 0; i<size; i++) {
+        cin >> num[i];
+    }
+    cout<<"The sum of all the elements in the array is: " << sumArr(num, size) << endl;
+}
+
